15378d39fd20e2b04b5b8aedbcc5058e32ccce3c
Hack Reactor Students <klp2012@gmail.com>
2014-08-18 17:21:49 -0700

    renders signup page

--- public/client/router.js
+++ public/client/router.js
@@ -5,17 +5,30 @@ Shortly.Router = Backbone.Router.extend({
 
   routes: {
     '':       'index',
-    'create': 'create'
+    'create': 'create',
+    'login':  'login'
   },
 
   swapView: function(view){
     this.$el.html(view.render().el);
   },
 
+  loginView: function() {
+    this.$el.html('login page');
+    console.log('loginView');
+  },
+
   index: function(){
     var links = new Shortly.Links();
     var linksView = new Shortly.LinksView({ collection: links });
     this.swapView(linksView);
+  },
+
+  login: function(){
+    // var links = new Shortly.Links();
+    // var linksView = new Shortly.LinksView({ collection: links });
+    console.log('login');
+    this.loginView();
   },
 
   create: function(){
--- shortly.js
+++ shortly.js
@@ -119,4 +119,5 @@ app.get('/*', function(req, res) {
 });
 
 console.log('Shortly is listening on 4568');
-app.listen(4568);
+// app.listen(4568);
+app.listen(8080);
