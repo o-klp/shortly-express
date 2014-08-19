7cebf7539d5b1638958cdd47d5fcd25775f3c173
Hack Reactor Students <klp2012@gmail.com>
2014-08-18 23:48:35 -0700

    creates hashing utility function

--- app/models/user.js
+++ app/models/user.js
@@ -2,21 +2,12 @@ var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
-var cryptThis = function(password, userContext) {
-  bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash(password, salt, function(err, hash) {
-      userContext.password = hash;
-    });
-  });
-};
-
-
 var User = db.Model.extend({
   tableName: 'users',
 
   hashThis: function() {
-    var salt = bcrypt.genSalt(10, function(err, salt) { return salt});
-    return Promise.promisify(bcrypt.hash)(this.password, salt, null)
+    // hashes password but does not salt (first null is salt)
+    return Promise.promisify(bcrypt.hash)(this.password, null, null)
                   .bind(this)
                   .then(function(hash) {
                     this.set('password', hash);
